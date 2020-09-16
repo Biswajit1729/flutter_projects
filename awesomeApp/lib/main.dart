0b1b3bea74f8c51bb442cc129d23f9d90c55568b
unknown <mandalbis1729@gmail.com>
2020-09-16 20:48:31 +0530

    second Chepter

--- awesomeApp/lib/main.dart
+++ awesomeApp/lib/main.dart
@@ -10,14 +10,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome App'),
-      ),
-      body: Container(
-        color: Colors.blueAccent,
-        child: Center(
-          child: Text(
-            'this is my first text',
+      appBar: AppBar(title: Text('Awesome App')),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+              //shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[400],
+                    blurRadius: 10,
+                    //spreadRadius: 1,
+                    offset: Offset(10.0, 10.0))
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+          child: Center(
+            child: Text(
+              'hello world',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
