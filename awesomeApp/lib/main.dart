8e0bcb29f23716b50efcdb6cd72ebf56b4099b6b
unknown <mandalbis1729@gmail.com>
2020-09-17 12:36:14 +0530

    cheapter 4 done

--- awesomeApp/lib/main.dart
+++ awesomeApp/lib/main.dart
@@ -16,18 +16,32 @@ class HomePage extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-              child: Text(
-                'hi i am drawer',
-                style: TextStyle(color: Colors.white),
+            // DrawerHeader(
+            //   child: Text(
+            //     'hi i am drawer',
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.indigo),
+            // ),
+            UserAccountsDrawerHeader(
+              accountName: Text('Biswajit mandal'),
+              accountEmail: Text('mandalbis1729@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
               ),
-              decoration: BoxDecoration(color: Colors.indigo),
             ),
             ListTile(
               leading: Icon(Icons.person),
               title: Text('Account'),
               subtitle: Text('persional'),
               trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('mandalbis1729@gmail.com'),
+              subtitle: Text('mail'),
+              trailing: Icon(Icons.send),
             )
           ],
         ),
