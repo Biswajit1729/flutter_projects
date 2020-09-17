dbb61eaf24a830918ef3a0ac50abf135b6f77205
unknown <mandalbis1729@gmail.com>
2020-09-17 12:11:07 +0530

    cheapter 4

--- awesomeApp/lib/main.dart
+++ awesomeApp/lib/main.dart
@@ -11,41 +11,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Awesome App')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  )
-                ],
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(
+                'hi i am drawer',
+                style: TextStyle(color: Colors.white),
               ),
+              decoration: BoxDecoration(color: Colors.indigo),
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Account'),
+              subtitle: Text('persional'),
+              trailing: Icon(Icons.edit),
+            )
+          ],
         ),
       ),
     );
